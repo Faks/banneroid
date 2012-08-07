88c1b5a453da4685f0c0ee95caa971607939a8cb
Jonh <Faks@users.noreply.github.com>
2012-08-08 01:35:19 +0300

    Pedejais rev beidzu 1:05

--- configuracijas/configuracija.php
+++ configuracijas/configuracija.php
@@ -1,13 +1,22 @@
 <?php
-session_start(); //izsauc datubÄ�zi
+#ini_set('display_errors', 'On');
+#error_reporting(E_ALL);
+#ini_set('display_errors', '1');
 
+session_start();
 
 require 'mysql_konekcija.php';
 
+if ($_GET['iziet'] == "ja") 
+{
+	session_destroy();
+	unset($_SESSION);
+}
 
 if ($_POST['ienakt']) 
 {
 	//drosibas filtrs bazisks nau nekads pasaules brinums
+	$vards = $_POST['vards'];
 	$vards = mysql_real_escape_string($_POST['vards']);
 	$vards = htmlspecialchars($_POST['vards']);
 
@@ -15,22 +24,21 @@ if ($_POST['ienakt'])
 	$kods = htmlspecialchars($_POST['kods']);
 	$kods = sha1($_POST['kods']);
 	
-	$panem_kodu = mysql_query("SELECT * FROM lietotaji WHERE vards = '{$vards}' ") or die(mysql_error());
-	if (mysql_num_rows($panem_kodu) > 0)
+	$panem_kodu = mysql_query("SELECT * FROM lietotaji WHERE vards = '{$_POST['vards']}' ") or die(mysql_error());
+	if (mysql_num_rows($panem_kodu) != 0)
 	{
 		$parbauda_kodu = mysql_fetch_array($panem_kodu);
 	}
 	else 
 	{
-		return false;	
+		$ienakt_neizdevas = true;
 	}
 	
 	
-	if ($_POST['kods'] == $parbauda_kodu['kods']) 
+	if ($kods == $parbauda_kodu['kods']) #te bija kluda labota jau
 	{
-		$dati = mysql_query("SELECT * FROM lietotaji WHERE vards= '{$vards}' AND kods = '{$kods} limit 1' ");
-		
-		
+		$dati = mysql_query("SELECT * FROM lietotaji WHERE vards= '".$vards."' AND kods = '".$kods."' limit 1 ") or die(mysql_error()); #kluda labota bija gluks
+
 		if (mysql_num_rows($dati) == 1) 
 		{
 			$mani_dati = mysql_fetch_array($dati);
