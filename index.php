315f8da4b9ed6cea2eb7d4537d1c0f6fa00ac676
graetz <graetz@GRAETZ-LAPTOPS>
2012-08-07 23:52:51 +0300

    login forma

--- index.php
+++ index.php
@@ -8,6 +8,11 @@ if ($_SESSION['ienacis'])
 else 
 {
 	echo "Ludzu ienac sistema";
+        echo "<form name='input' action='configuracijas/configuracija.php' method='post'>
+            Lietotājvārds: <input type='text' name='vards' /></br />
+            Parole: <input type='password' name='kods' /><br />
+            <input type='submit' name='ienakt' value='Ienākt' />
+        </form>";
 }	
 
 ?>
