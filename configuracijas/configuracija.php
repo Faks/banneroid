b96d420b067a68eebf274b727decdf8419b45f7c
graetz <graetz@users.noreply.github.com>
2012-08-08 13:02:37 +0300

    style fix

--- configuracijas/configuracija.php
+++ configuracijas/configuracija.php
@@ -1,5 +1,5 @@
 <?php
-#ini_set('display_errors', 'On');
+ini_set('display_errors', 'On');
 #error_reporting(E_ALL);
 #ini_set('display_errors', '1');
 
@@ -30,36 +30,38 @@ if (isset($_POST['ienakt']))
         {
             $ienakt_neizdevas = true;
         }
+        
 	$panem_kodu = mysql_query("SELECT * FROM lietotaji WHERE vards = '{$_POST['vards']}' ") or die(mysql_error());
 	if (mysql_num_rows($panem_kodu) != 0)
 	{
-		$parbauda_kodu = mysql_fetch_array($panem_kodu);
+            $parbauda_kodu = mysql_fetch_array($panem_kodu);
 	}
 	else 
 	{
-		$ienakt_neizdevas = true;
+            $ienakt_neizdevas = true;
 	}
+        
 	if(!empty($_POST['kods'])){
-	if ($kods == $parbauda_kodu['kods']) #te bija kluda labota jau
-	{
-		$dati = mysql_query("SELECT * FROM lietotaji WHERE vards= '".$vards."' AND kods = '".$kods."' limit 1 ") or die(mysql_error()); #kluda labota bija gluks
+            if ($kods == $parbauda_kodu['kods']) #te bija kluda labota jau
+            {
+                    $dati = mysql_query("SELECT * FROM lietotaji WHERE vards= '".$vards."' AND kods = '".$kods."' limit 1 ") or die(mysql_error()); #kluda labota bija gluks
 
-		if (mysql_num_rows($dati) == 1) 
-		{
-			$mani_dati = mysql_fetch_array($dati);
-			$_SESSION['ienacis'] = true; //labojums bija kluda :)
-			
-			$_SESSION['vards'] = $mani_dati['vards'];
-		}
-	}
-	else 
-	{
-		$ienakt_neizdevas = true;
-	}
+                    if (mysql_num_rows($dati) == 1) 
+                    {
+                        $mani_dati = mysql_fetch_array($dati);
+                        $_SESSION['ienacis'] = true; //labojums bija kluda :)
+
+                        $_SESSION['vards'] = $mani_dati['vards'];
+                    }
+            }
+            else 
+            {
+                $ienakt_neizdevas = true;
+            }
         }
         else 
 	{
-		$ienakt_neizdevas = true;
+            $ienakt_neizdevas = true;
 	}
 }
 ?>
