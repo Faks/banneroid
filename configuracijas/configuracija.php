719d1c5ea9ff2b3fcc76c4f01cb754bf365d2269
graetz <graetz@localhost>
2012-08-08 18:09:50 +0300

    login hotfix

--- configuracijas/configuracija.php
+++ configuracijas/configuracija.php
@@ -9,7 +9,9 @@ require 'mysql_konekcija.php';
 if(isset($_GET['iziet'])){
     if ($_GET['iziet'] == "ja") 
     {
+        session_unset();
 	session_destroy();
+        header('Location: ./');
 	unset($_SESSION);
     }
 }
@@ -34,13 +36,13 @@ if(isset($_POST['reg'])){
             $varda_parbaude=mysql_query("SELECT `vards` FROM `lietotaji` WHERE `vards`='{$vards}'");
             if(mysql_num_rows($varda_parbaude) > 0)
             {
-                echo 'Šis lietotāj vārds ir aizņemts!';
+                echo 'Šis lietotāja vārds jau ir aizņemts!';
             }
             else 
             {
                 $datums=date('Y-m-d H:i:s');
                 mysql_query("INSERT INTO `lietotaji`(`vards`, `kods`, `pievienojas`, `limenis`)VALUES('{$vards}','{$parole}','{$datums}','lietotajs')");
-                echo 'Reģistrācija veiksmīga tagad variet ielogoties sistēmā!<br />';
+                echo 'Reģistrācija veiksmīga tagad variet <a href="./">ielogoties</a> sistēmā!<br />';
             }
         }
         else
@@ -54,7 +56,9 @@ if(isset($_POST['reg'])){
 if (isset($_POST['ienakt'])) 
 {
 	//drosibas filtrs bazisks nau nekads pasaules brinums
-        if(!empty($_POST['vards']) || !empty($_POST['kods'])){
+        if(empty($_POST['vards']) || empty($_POST['kods'])){
+            echo 'Jāaizpilda abas ailes';
+        }else{
             $vards = $_POST['vards'];
             $vards = mysql_real_escape_string($_POST['vards']);
             $vards = htmlspecialchars($_POST['vards']);
@@ -62,14 +66,10 @@ if (isset($_POST['ienakt']))
             $kods = mysql_real_escape_string($_POST['kods']);
             $kods = htmlspecialchars($_POST['kods']);
             $kods = sha1($_POST['kods']);
-        }
-        else
-        {
-            $ienakt_neizdevas = true;
-        }
+
         
 	$panem_kodu = mysql_query("SELECT * FROM lietotaji WHERE vards = '{$vards}' ") or die(mysql_error());
-	if (mysql_num_rows($panem_kodu) != 0)
+	if (mysql_num_rows($panem_kodu) > 0)
 	{
             $parbauda_kodu = mysql_fetch_array($panem_kodu);
 	}
@@ -78,7 +78,6 @@ if (isset($_POST['ienakt']))
             $ienakt_neizdevas = true;
 	}
         
-	if(!empty($_POST['kods'])){
             if ($kods == $parbauda_kodu['kods']) #te bija kluda labota jau
             {
                     $dati = mysql_query("SELECT * FROM lietotaji WHERE vards= '".$vards."' AND kods = '".$kods."' limit 1 ") or die(mysql_error()); #kluda labota bija gluks
@@ -95,10 +94,7 @@ if (isset($_POST['ienakt']))
             {
                 $ienakt_neizdevas = true;
             }
-        }
-        else 
-	{
-            $ienakt_neizdevas = true;
-	}
+
+}
 }
 ?>
