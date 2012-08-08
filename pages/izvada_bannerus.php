5877ae796df83036ad129202e646e06eb10e2225
Jonh <Faks@users.noreply.github.com>
2012-08-08 18:37:39 +0300

    kaut kads gluks nevada ara

---
+++ pages/izvada_bannerus.php
@@ -0,0 +1,22 @@
+<?php
+$select_bildes_skaititaju = mysql_query("SELECT * FROM bannera_sistema") or die(mysql_error());
+while($bildes_skaititajs = mysql_fetch_array($select_bildes_skaititaju))
+{
+	if (mysql_num_rows($bildes_skaititajs) > 0) 
+	{
+		echo "<div class='pagination'>".$bildes_skaititajs['bannera_kods'].'<br>bilde izvadas<br>';
+		
+		$select_counter_bildes = mysql_query("SELECT COUNT(id) FROM bannera_sistema_lietotaji WHERE bannera_id = '".$bildes_skaititajs['id']."' ") or die(mysql_error());
+		$counter_bildes = mysql_fetch_array($select_counter_bildes);
+		
+		echo 'Izvada ka ciparus<br>bildi izmanto '.$counter_bildes['COUNT(id)'].' cilveki<br>';
+		
+		echo "<br>Izvada cik lieto kad uzbrauc bildes<br><a href='http://faks.sytes.net'><img src='http://faks.sytes.net/ics.gif' width='88' height='31' title='bildi izmanto {$counter_bildes['COUNT(id)']} cilveki' /></a><br>";
+	}
+	else
+	{
+		echo "Nau Ko izvadit";
+	}
+}
+
+?>
