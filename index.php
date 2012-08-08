f06e74c013355a04910f0e7ac5c6c5eae2447727
graetz <graetz@GRAETZ-LAPTOPS>
2012-08-08 11:45:43 +0300

    error fix un izmainita login forma

--- index.php
+++ index.php
@@ -1,33 +1,40 @@
 <?php
+echo '<!DOCTYPE html>
+<html lang="lv">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+        <link rel="stylesheet" type="text/css" href="style.css"/>
+	<title>Baneroid</title>
+</head>
+<body>';
 require 'configuracijas/configuracija.php';
 
 $test = sha1("test");
 #echo $test;
 	
-	if ($ienakt_neizdevas)
+	if (isset($ienakt_neizdevas))
 	{
-		echo "vards vai kods nepareizs";
+		echo "Lietotāja vārds vai parole ir nepareizs";
 	}
 	
-	if (!$_SESSION['ienacis'])
+	if (!isset($_SESSION['ienacis']))
 	{
 	        echo "
-	        	<form name='input' method='post'>
-	        		
-	            <label>Lietot�ja V�rds:</label>
-	        		<input type='text' name='vards' /></br />
-	            <label>Parole:</label> 
-	       	  		<input type='password' name='kods' /><br />
-	        		
-	            <input type='submit' name='ienakt' value='ienakt' />
-	        </form>";
+                <div id='login'>
+	        <form name='input' method='post'>	
+	            <label>Lietotāja Vārds:</label><input type='text' name='vards' /></br />
+	            <label>Parole:</label><input type='password' name='kods' /><br />	
+	            <input type='submit' name='ienakt' value='Ienākt' />
+	        </form>
+                </div>";
 	}
 	else
 	{
-		echo $_SESSION['vards']."<br><a href='?iziet=ja'>Iziet</a>";
+		echo $_SESSION['vards']."<br /><a href='?iziet=ja'>Iziet</a>";
 	}
 
 	#print_r($dati);
         	
-
+echo '</body>
+</html>';
 ?>
