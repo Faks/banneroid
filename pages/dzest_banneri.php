8ecf34d28a82cdaac4f37952ccdc7c6de43a86cf
graetz <graetz@localhost>
2012-08-10 14:10:54 +0300

    test

--- pages/dzest_banneri.php
+++ pages/dzest_banneri.php
@@ -5,7 +5,7 @@ if ((int)$_GET['id'])
 	$autors = $_GET['autors'];
 	if (mysql_query("DELETE FROM bannera_sistema WHERE id = '".$id."' AND autors = '".$autors."' ")) 
 	{
-		header('Location: /?sadala=izvada_bannerus'); #nedarbojas ka vajaga hmm
+		header('Location: ./?sadala=izvada_bannerus'); #nedarbojas ka vajaga hmm
 	}
 }
 else 
